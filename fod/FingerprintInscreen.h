2ead6a5462baebaa24dab8e8e65b89e3284cb7c0
XiNGRZ <chenxingyu92@gmail.com>
2019-12-28 01:47:21 +0800

    Revert "sm8150: fod: Force HBM during FOD is shown"
    This reverts commit b84229755b8b136053d3c06bb2f4ae7882ceff68.
    
    Change-Id: Id9e867cf36ba9931656c4193878b47f256cc3950

--- fod/FingerprintInscreen.h
+++ fod/FingerprintInscreen.h
@@ -61,7 +61,6 @@ class FingerprintInscreen : public IFingerprintInscreen {
     std::mutex mCallbackLock;
     sp<IFingerprintInscreenCallback> mCallback;
 
-    int32_t mHBM;
     bool mIconShown;
     bool mFingerPressed;
 
