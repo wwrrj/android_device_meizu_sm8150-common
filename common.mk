216973c4cb21dd7349ccfca38c4f152efbd17ae4
XiNGRZ <chenxingyu92@gmail.com>
2020-01-03 23:07:42 +0800

    sm8150: audio: Pull audio configs from stock
    Change-Id: I5508b3285f6399938da8833dd16e8d2b34951aec

--- common.mk
+++ common.mk
@@ -39,6 +39,9 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     libaacwrapper
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_policy_configuration.xml
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
