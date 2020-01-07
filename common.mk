42106f4f68acdd2081aeaeade4610071a47330ab
XiNGRZ <chenxingyu92@gmail.com>
2020-01-07 15:50:48 +0800

    sm8150: Introduce custom LiveDisplay implementation
    Change-Id: I27de69809674482c48e70d002008098cf794418c

--- common.mk
+++ common.mk
@@ -58,6 +58,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.meizu_sm8150
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    mokee.livedisplay@2.0-service-meizu_sm8150
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
