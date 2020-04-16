214c06d8b7644f10ac422fc251fa18ac6dc5388f
XiNGRZ <chenxingyu92@gmail.com>
2020-04-17 02:45:34 +0800

    sm8150: Ship android.hardware.telephony.ims.xml
    Change-Id: Ia367f763342d3ed914fc5ebb1e2ef7e447e9bdee

--- common.mk
+++ common.mk
@@ -24,6 +24,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
     vendor/mokee/config/permissions/vendor.mokee.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.mokee.biometrics.fingerprint.inscreen.xml
 
